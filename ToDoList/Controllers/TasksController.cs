828f82d48d440e5deb4753f294ef722dc57ff748
Assylbekova Gulzhan <123gulzhan@gmail.com>
2021-03-04 19:54:43 +0600

    init

---
+++ ToDoList/Controllers/TasksController.cs
@@ -0,0 +1,15 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace ToDoList.Controllers
+{
+    public class TasksController : Controller
+    {
+        
+        
+        // GET
+        public IActionResult Index()
+        {
+            return View();
+        }
+    }
+}
