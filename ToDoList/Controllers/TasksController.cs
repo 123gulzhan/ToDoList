366b55aaa73a2a6616fc167d88b499fc33b8fe3d
Assylbekova Gulzhan <123gulzhan@gmail.com>
2021-03-12 21:00:43 +0600

    вывела отдельно метод сортировки, добавлены placeholder для фильтрации даты

--- ToDoList/Controllers/TasksController.cs
+++ ToDoList/Controllers/TasksController.cs
@@ -2,11 +2,11 @@
 using System.Collections.Generic;
 using System.Linq;
 using Microsoft.AspNetCore.Mvc;
-using Microsoft.AspNetCore.Mvc.Rendering;
 using Microsoft.EntityFrameworkCore;
 using ToDoList.Enums;
 using ToDoList.Models;
 using ToDoList.ViewModels;
+using Task = ToDoList.Models.Task;
 
 namespace ToDoList.Controllers
 {
@@ -19,55 +19,38 @@ namespace ToDoList.Controllers
             _db = db;
         }
 
-        public IActionResult Index(string header, string createFrom, string createTo, 
-            string priority, string status, string descriptionPart, 
+        public IActionResult Index(string header, string createFrom, string createTo,
+            string priority, string status, string descriptionPart,
             SortState sortOrder = SortState.HeaderAsc)
         {
-            
+
             List<Task> tasks = _db.Tasks.ToList();
-            IQueryable<Task> tasksQ = tasks.AsQueryable(); 
+            tasks = SortedTask(tasks, sortOrder);
 
-            ViewBag.HeaderSort = sortOrder == SortState.HeaderAsc ? SortState.HeaderDesc : SortState.HeaderAsc;
-            ViewBag.PrioritySort = sortOrder == SortState.PriorityAsc ? SortState.PriorityDesc : SortState.PriorityAsc;
-            ViewBag.StatusSort = sortOrder == SortState.StatusAsc ? SortState.StatusDesc : SortState.StatusAsc;
-            ViewBag.CreateSort = sortOrder == SortState.CreateAsc ? SortState.CreateDesc : SortState.CreateAsc;
-
-            switch (sortOrder)
-            {
-                case SortState.HeaderDesc: tasksQ = tasksQ.OrderByDescending(t => t.Header); break;
-                case SortState.PriorityAsc: tasksQ = tasksQ.OrderBy(t => t.Priority); break;
-                case SortState.PriorityDesc: tasksQ = tasksQ.OrderByDescending(t => t.Priority); break;
-                case SortState.StatusAsc: tasksQ = tasksQ.OrderBy(t => t.Status); break;
-                case SortState.StatusDesc: tasksQ = tasksQ.OrderByDescending(t => t.Status); break;
-                case SortState.CreateAsc: tasksQ = tasksQ.OrderBy(t => t.CreationAt); break;
-                case SortState.CreateDesc: tasksQ = tasksQ.OrderByDescending(t => t.CreationAt); break;
-                default: tasksQ = tasksQ.OrderBy(t => t.Header); break;
-            }
-
-            tasks = tasksQ.ToList();
-            
             FilterViewModel model = new FilterViewModel
             {
-                Tasks = tasks.ToList(),
+                Tasks = tasks,
                 Header = header,
                 CreationFrom = Convert.ToDateTime(createFrom),
                 CreationTo = Convert.ToDateTime(createTo),
-                Priority = priority == "1" ? Priority.Высокий 
-                    : (priority == "2" ? Priority.Средний 
+                Priority = priority == "1" ? Priority.Высокий
+                    : (priority == "2" ? Priority.Средний
                         : (priority == "3" ? Priority.Низкий : Priority.Все)),
                 Status = status == "1" ? Status.Новая
                     : (status == "2" ? Status.Открытая
                         : (status == "3" ? Status.Закрытая : Status.Все))
             };
-            if(descriptionPart != null)
+
+            if (descriptionPart != null)
             {
                 model.Tasks = model.Tasks.FindAll(task => task.Description.Contains(descriptionPart));
             }
-            model.Tasks = Filtration(model);
+
+            model.Tasks = FilteredTask(model);
             return View(model);
         }
 
-        
+
         [HttpGet]
         public IActionResult Create()
         {
@@ -76,7 +59,7 @@ namespace ToDoList.Controllers
             return View();
         }
 
-        
+
         [HttpPost]
         public IActionResult Create(Task task)
         {
@@ -87,10 +70,11 @@ namespace ToDoList.Controllers
                 _db.SaveChanges();
                 return RedirectToAction("Index");
             }
+
             return RedirectToAction("Create");
         }
 
-        
+
         [HttpGet]
         public IActionResult Delete(int id)
         {
@@ -101,12 +85,14 @@ namespace ToDoList.Controllers
                 {
                     return View(task);
                 }
+
                 return NotFound();
             }
+
             return NotFound();
         }
 
-        
+
         [HttpPost]
         [ActionName("Delete")]
         public IActionResult ConfirmDelete(int id)
@@ -117,10 +103,11 @@ namespace ToDoList.Controllers
                 _db.Entry(task).State = EntityState.Deleted;
                 _db.SaveChanges();
             }
+
             return RedirectToAction("Index");
         }
 
-        
+
         [HttpGet]
         public IActionResult Details(int id)
         {
@@ -128,10 +115,11 @@ namespace ToDoList.Controllers
             {
                 return NotFound();
             }
+
             Task task = _db.Tasks.FirstOrDefault(t => t.Id == id);
             return View(task);
         }
-        
+
         public IActionResult ChangeStatus(int id, string status)
         {
             if (id != 0)
@@ -141,7 +129,7 @@ namespace ToDoList.Controllers
                 {
                     switch (status)
                     {
-                        case"Open":
+                        case "Open":
                             if (task.Status == Status.Закрытая || task.Status == Status.Открытая)
                             {
                                 return RedirectToAction("Index");
@@ -149,7 +137,7 @@ namespace ToDoList.Controllers
 
                             task.Status = Status.Открытая;
                             break;
-                        case"Close":
+                        case "Close":
                             if (task.Status == Status.Закрытая || task.Status == Status.Новая)
                             {
                                 return RedirectToAction("Index");
@@ -164,12 +152,13 @@ namespace ToDoList.Controllers
                     _db.SaveChanges();
                 }
             }
+
             return RedirectToAction("Index");
         }
 
-        
+
         [NonAction]
-        public List<Task> Filtration(FilterViewModel model)
+        public List<Task> FilteredTask(FilterViewModel model)
         {
             if (model.Header != null)
             {
@@ -179,21 +168,21 @@ namespace ToDoList.Controllers
             if (model.CreationFrom.Date != DateTime.Parse("01.01.0001"))
             {
                 model.Tasks = model.Tasks.Where(t => t.CreationAt.Date >= Convert
-                        .ToDateTime(model.CreationFrom.Date)).ToList();
+                    .ToDateTime(model.CreationFrom.Date)).ToList();
             }
 
             if (model.CreationTo.Date != DateTime.Parse("01.01.0001"))
             {
                 model.Tasks = model.Tasks.Where(t => t.CreationAt <= Convert
-                        .ToDateTime(model.CreationTo.Date)).ToList();
+                    .ToDateTime(model.CreationTo.Date)).ToList();
             }
 
-            if(model.Priority != Priority.Все)
+            if (model.Priority != Priority.Все)
             {
                 model.Tasks = model.Tasks.Where(t => t.Priority == model.Priority).ToList();
             }
 
-            if(model.Status != Status.Все)  
+            if (model.Status != Status.Все)
             {
                 model.Tasks = model.Tasks.Where(t => t.Status == model.Status).ToList();
             }
@@ -202,7 +191,49 @@ namespace ToDoList.Controllers
             {
                 return new List<Task>();
             }
+
             return model.Tasks;
+        }
+
+        [NonAction]
+        public List<Task> SortedTask(List<Task> tasks, SortState sortOrder)
+        {
+            IQueryable<Task> tasksQ = tasks.AsQueryable();
+
+            ViewBag.HeaderSort = sortOrder == SortState.HeaderAsc ? SortState.HeaderDesc : SortState.HeaderAsc;
+            ViewBag.PrioritySort = sortOrder == SortState.PriorityAsc ? SortState.PriorityDesc : SortState.PriorityAsc;
+            ViewBag.StatusSort = sortOrder == SortState.StatusAsc ? SortState.StatusDesc : SortState.StatusAsc;
+            ViewBag.CreateSort = sortOrder == SortState.CreateAsc ? SortState.CreateDesc : SortState.CreateAsc;
+
+            switch (sortOrder)
+            {
+                case SortState.HeaderDesc:
+                    tasksQ = tasksQ.OrderByDescending(t => t.Header);
+                    break;
+                case SortState.PriorityAsc:
+                    tasksQ = tasksQ.OrderBy(t => t.Priority);
+                    break;
+                case SortState.PriorityDesc:
+                    tasksQ = tasksQ.OrderByDescending(t => t.Priority);
+                    break;
+                case SortState.StatusAsc:
+                    tasksQ = tasksQ.OrderBy(t => t.Status);
+                    break;
+                case SortState.StatusDesc:
+                    tasksQ = tasksQ.OrderByDescending(t => t.Status);
+                    break;
+                case SortState.CreateAsc:
+                    tasksQ = tasksQ.OrderBy(t => t.CreationAt);
+                    break;
+                case SortState.CreateDesc:
+                    tasksQ = tasksQ.OrderByDescending(t => t.CreationAt);
+                    break;
+                default:
+                    tasksQ = tasksQ.OrderBy(t => t.Header);
+                    break;
+            }
+
+            return tasksQ.ToList();
         }
     }
 }
